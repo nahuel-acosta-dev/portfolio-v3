cbf6108ff633b65de354a0f2b304b8226faa0864
Nahuel <nahuel.acosta@khemlabs.com>
2025-08-23 23:52:27 -0300

    change in layour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Bebas_Neue, Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar/NavBar";
 import Preloader from "@/ui/PreLoader";
-import { initI18nServer } from "@/lib/i18n/server";
-import { languages, fallbackLng } from "@/lib/i18n/config";
 
 export const metadata: Metadata = {
   title: "Nahuel Acosta | Portfolio",
@@ -25,31 +23,13 @@ const poppins = Poppins({
   variable: "--font-poppins",
 });
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
-  params: { lng },
 }: Readonly<{
   children: React.ReactNode;
-  params: { lng: string };
 }>) {
-  const currentLng = languages.includes(lng) ? lng : fallbackLng;
-
-  // 1) Inicializás i18n en el server con el locale correcto
-  const i18n = await initI18nServer(currentLng, ["translation"]);
-
-  // 2) Extraés el "snapshot" de recursos para hidratar el cliente
-  const resources = i18n.services.resourceStore.data;
-  const initial = JSON.stringify({ lng: currentLng, resources });
   return (
-    <html lang={currentLng} suppressHydrationWarning>
-      <head>
-        {/* snapshot embebido para el cliente */}
-        <script
-          id="__I18N_SNAPSHOT__"
-          type="application/json"
-          dangerouslySetInnerHTML={{ __html: initial }}
-        />
-      </head>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${bebas.variable} ${poppins.variable} bg-darkbg text-white`}
       >
